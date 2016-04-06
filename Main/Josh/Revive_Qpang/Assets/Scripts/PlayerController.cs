b0693be3049304f47f03f8029c38cbfcc728f501
JoshNC <joshuanh@hotmail.nl>
2016-04-06 22:17:07 +0200

    Progress report #2
    Added to personal folder:
    - Debug log in screen @ player movement
    - Jumping

--- Main/Josh/Revive_Qpang/Assets/Scripts/PlayerController.cs
+++ Main/Josh/Revive_Qpang/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -14,7 +15,7 @@ public class PlayerController : NetworkBehaviour
     public float playerSpeed_Backwards = 5f;
     public float playerSpeed_RunMultiplier = 2f;
     public float playerSpeed_SlowMultiplier = 2f;
-    public float playerJump_Force = 30f;
+    public float playerJump_Force = 50f;
     public float LookSensitivity = 5f;
 
     public KeyCode RunKey = KeyCode.LeftShift;
@@ -26,6 +27,21 @@ public class PlayerController : NetworkBehaviour
     public static bool player_isGrounded;
     public static bool player_isSlowWalking;
 
+    public static float currentPlayerForwardSpeed = 0;
+    public static float currentPlayerStrafeSpeed = 0;
+    public static float currentPlayerBackwardsSpeed = 0;
+    public static float mouseSensitivity = 0;
+    public static float currentPlayerPosX = 0;
+    public static float currentPlayerPosY = 0;
+    public static float currentPlayerPosZ = 0;
+    public static float currentJumpForce = 0;
+    public static float runMultiplier = 0;
+    public static float slowMultiplier = 0;
+
+    //private fields
+    private float moveCounter = 1f;
+
+    private Rigidbody rB;
 
     // Use this for initialization
     void Start ()
@@ -33,6 +49,13 @@ public class PlayerController : NetworkBehaviour
         player_isRunning = false;
         player_isGrounded = true;
         player_isSlowWalking = false;
+
+        mouseSensitivity = LookSensitivity;
+        runMultiplier = playerSpeed_RunMultiplier;
+        slowMultiplier = playerSpeed_SlowMultiplier;
+        currentJumpForce = playerJump_Force;
+
+        rB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -41,6 +64,11 @@ public class PlayerController : NetworkBehaviour
         // ==============================================\
         // Movement
         // ==============================================\
+        // player position
+        currentPlayerPosX = gameObject.GetComponent<Rigidbody>().position.x;
+        currentPlayerPosY = gameObject.GetComponent<Rigidbody>().position.y;
+        currentPlayerPosZ = gameObject.GetComponent<Rigidbody>().position.z;
+
         // Calculate movement velocity as a 3D vector
         float _xMove = Input.GetAxisRaw("Horizontal");
         float _zMove = Input.GetAxisRaw("Vertical");
@@ -48,11 +76,71 @@ public class PlayerController : NetworkBehaviour
         Vector3 _moveHorizontal = transform.right * _xMove * playerSpeed_Strafe;
         Vector3 _moveVertical = transform.forward * _zMove * playerSpeed_Forward;
 
+        // z1: voor, z-1: achter | x1: rechts, x-1 links
+        // Speed slope
+        if ((Input.GetAxisRaw("Horizontal") == 1) || (Input.GetAxisRaw("Horizontal") == -1)) // right
+        {
+            if (moveCounter < playerSpeed_Strafe) moveCounter += 0.08f + moveCounter;
+            if (moveCounter > playerSpeed_Strafe)
+            {
+                _moveHorizontal = transform.right * _xMove * playerSpeed_Strafe;
+                currentPlayerStrafeSpeed = _xMove * playerSpeed_Strafe;
+            } else
+            {
+                _moveHorizontal = transform.right * _xMove * moveCounter;
+                currentPlayerStrafeSpeed = _xMove * moveCounter;
+            }
+        }
+        else if(Input.GetAxisRaw("Vertical") == 1) // Forward
+        {
+            if (moveCounter < playerSpeed_Forward) moveCounter += 0.08f + moveCounter;
+            if (moveCounter > playerSpeed_Forward)
+            {
+                _moveVertical = transform.forward * _zMove * playerSpeed_Forward;
+                currentPlayerForwardSpeed = _zMove * playerSpeed_Forward;
+            }
+            else
+            {
+                _moveVertical = transform.forward * _zMove * moveCounter;
+                currentPlayerForwardSpeed = _zMove * moveCounter;
+            }
+        }
+        else if (Input.GetAxisRaw("Vertical") == -1) // Backwards
+        {
+            if (moveCounter < playerSpeed_Backwards) moveCounter += 0.08f + moveCounter;
+            if (moveCounter > playerSpeed_Backwards)
+            {
+                _moveVertical = transform.forward * _zMove * playerSpeed_Backwards;
+                currentPlayerBackwardsSpeed = _zMove * playerSpeed_Backwards;
+            }
+            else
+            {
+                _moveVertical = transform.forward * _zMove * moveCounter;
+                currentPlayerBackwardsSpeed = _zMove * moveCounter;
+            }
+        }
+        else
+        {
+            moveCounter = 0;
+            currentPlayerStrafeSpeed = _xMove * moveCounter;
+            currentPlayerForwardSpeed = _zMove * moveCounter;
+            currentPlayerBackwardsSpeed = _zMove * moveCounter;
+        }
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (player_isGrounded)
+            {
+                rB.AddForce(Vector3.up * playerJump_Force);
+            }
+        }
+
+
         // Final movement vector
         Vector3 _velocityGrounded = (_moveHorizontal + _moveVertical);
 
         GetComponent<PlayerMovement>().Move(_velocityGrounded);
-
 
         // ==============================================\
         // Camera look
@@ -72,5 +160,15 @@ public class PlayerController : NetworkBehaviour
 
         // apply  rotation
         GetComponent<PlayerMovement>().RotateCamera(_cameraRotation);
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        player_isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        player_isGrounded = false;
     }
 }
---
+++ Main/Josh/Revive_Qpang/Assets/Scripts/debugLog.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerController))]
+public class debugLog : MonoBehaviour {
+
+    public GameObject forwardSpeed;
+    public GameObject strafeSpeed;
+    public GameObject backwardsSpeed;
+    public GameObject mouseSensitivity;
+    public GameObject posX;
+    public GameObject posY;
+    public GameObject posZ;
+    public GameObject jumpForce;
+    public GameObject runMultiplier;
+    public GameObject slowMultiplier;
+    public GameObject keysPressed;
+    public GameObject isGrounded;
+    public GameObject isRunning;
+    public GameObject isSlowWalking;
+
+    private PlayerController pC;
+
+    // Use this for initialization
+    void Start () {
+        pC = GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        forwardSpeed.GetComponent<Text>().text = "Forward speed: " + PlayerController.currentPlayerForwardSpeed;
+        strafeSpeed.GetComponent<Text>().text = "Strafe speed: " + PlayerController.currentPlayerStrafeSpeed;
+        backwardsSpeed.GetComponent<Text>().text = "Backwards Speed: " + +PlayerController.currentPlayerBackwardsSpeed;
+        mouseSensitivity.GetComponent<Text>().text = "Mouse sensitivity: " + PlayerController.mouseSensitivity;
+        posX.GetComponent<Text>().text = "Pos x: " + PlayerController.currentPlayerPosX;
+        posY.GetComponent<Text>().text = "Pos Y: " + PlayerController.currentPlayerPosY;
+        posZ.GetComponent<Text>().text = "Pos Z: " + PlayerController.currentPlayerPosZ;
+        jumpForce.GetComponent<Text>().text = "Jump force: " + PlayerController.currentJumpForce;
+        runMultiplier.GetComponent<Text>().text = "Run multiplier: " + PlayerController.runMultiplier;
+        slowMultiplier.GetComponent<Text>().text = "Slow multiplier: " + PlayerController.slowMultiplier;
+        isGrounded.GetComponent<Text>().text = "Is grounded: " + PlayerController.player_isGrounded.ToString();
+        isRunning.GetComponent<Text>().text = "Is running: " + PlayerController.player_isRunning.ToString();
+        isSlowWalking.GetComponent<Text>().text = "Is SlowWalking: " + PlayerController.player_isSlowWalking.ToString();
+    }
+}
